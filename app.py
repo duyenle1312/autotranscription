b8ae8b8480edb7cfb47de150a80163b16996f3f4
Duyen Le <lengomyduyen@gmail.com>
2022-10-14 16:27:19 +0300

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
-value = os.getenv("API_KEY")
-print(value)
+# load_dotenv()
+# value = os.getenv("API_KEY")
+# print(value)
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="YouTube Transcription", page_icon=":book:")
 
 if 'status' not in st.session_state:
     st.session_state['status'] = 'submitted'
@@ -26,22 +29,26 @@ def refresh_state():
 
 st.title('Easily transcribe YouTube videos')
 
-link = st.text_input('Enter your YouTube video link', 'https://youtu.be/dccdadl90vs', on_change=refresh_state)
-st.video(link)
+col1, col2 = st.columns(2)
 
-st.text("The transcription is " + st.session_state['status'])
+with col1:
+	link = st.text_input('Enter your YouTube video link', 'https://youtu.be/dccdadl90vs', on_change=refresh_state)
+	st.video(link)
 
-polling_endpoint = transcribe_from_link(link, False)
+with col2:
+	st.text("The transcription is " + st.session_state['status'])
 
-st.button('check_status', on_click=get_status, args=(polling_endpoint,))
+	polling_endpoint = transcribe_from_link(link, False)
 
-transcript=''
-if st.session_state['status']=='completed':
-	f = open("transcript.txt")
-	transcript = f.read()
-	f.close()
+	st.button('check_status', on_click=get_status, args=(polling_endpoint,))
 
-st.markdown(transcript)
+	transcript=''
+	if st.session_state['status']=='completed':
+		f = open("transcript.txt")
+		transcript = f.read()
+		f.close()
+
+	st.markdown(transcript)
 
 
 
